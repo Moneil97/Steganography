babf23fb57a947cf0ac91155234607064fdfe735
Moneil <cam8059@yahoo.com>
2015-11-26 00:55:03 -0600

    Huge performance boost for larger files, fixed issues with multibit()

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -10,128 +10,67 @@ import javax.imageio.ImageIO;
 
 public class Encrypt {
 
+	static BufferedImage img;
+	static int bitsPerByteNeeded;
+	
 	public static void main(String[] args) {
 		
-		BufferedImage img = null;
+		
 		try {
-		    img = ImageIO.read(new File("src/qr.png")); //Open Image
+		    img = ImageIO.read(new File("src/testImage.png")); //Open Image
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		//String message = "Hello there, this is a secret message" + (char)0; //Add Null Terminator
+		long imageSize = img.getWidth() * img.getHeight() * 3;
+		say("imageSize: " + imageSize);
 		
-		String message = "";
-		long messageBits = 0;// = message.length()*8; 
+		long messageBits = 0;
 		try {
-			//Scanner scan = new Scanner(new File("src/input2.txt"));
 			
-			File f = new File("src/Shakespeare.txt");
-			messageBits = f.length() *8; 
-			
-			Scanner scan = new Scanner(new File("src/Shakespeare.txt"));
-			int a=0;
-			while (scan.hasNextLine() && a < 100){
-				message += scan.nextLine() + "\n";
-				say(a++);
+			File f = new File("src/input2.txt");
+			//File f = new File("src/Shakespeare.txt");
+			messageBits = (f.length()+1)*8; //+1 for Null Terminator
+			say("messageBits: " + messageBits);
+			bitsPerByteNeeded = (int) (messageBits/(imageSize-3)+1); //-3 to account for pixel 0,0 being used
+			say("Bits per byte needed: " + bitsPerByteNeeded);
+			if (bitsPerByteNeeded > 8){
+				System.err.println("image too small");
+				System.exit(0);
 			}
-			message +=(char)0;
+			//@ pixel 0,0 set last 4 binary digits (1R1G2B) to bitsPerByteNeeded (4 bit == 0-15)
+			img.setRGB(0, 0, setRGBbits(img.getRGB(0, 0), bitsPerByteNeeded, 4));
+			
+			Scanner scan = new Scanner(f);
+			scan.close();
+			scan = new Scanner(f);
+			//int a=0;
+			while (scan.hasNextLine()){
+				//say(a++);
+				
+				if (bitsPerByteNeeded == 1){
+					singleBit(scan.nextLine() + "\n");
+				}
+				else{
+					multiBit(scan.nextLine() + "\n");
+				}
+			}
+			
+			//Add Null Terminator for decrypter to use
+			if (bitsPerByteNeeded == 1){
+				singleBit(String.valueOf((char)0));
+			}
+			else{
+				multiBit(String.valueOf((char)0));
+				if (endBit.length() > 0)
+					img.setRGB(x, y, setRGBbits(img.getRGB(x, y), Integer.parseInt(endBit,2), endBit.length()));
+			}
+			
 			scan.close();
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 			System.exit(0);
 		}
-		
-		//int messageBits = message.length()*8; 
-		say("Bits: " + messageBits);
-		long imageSize = img.getWidth() * img.getHeight() * 3;
-		say("Image size: " + imageSize);
-		int bitsPerByteNeeded = (int) (messageBits/(imageSize-1)+1); //-1 to account for number added pixel 0,0
-		
-		if (bitsPerByteNeeded > 8){
-			System.err.println("image too small");
-			System.exit(0);
-		}
-		
-		say("Bits per byte needed: " + bitsPerByteNeeded);
-		
-		//@ pixel 0,0 set last 4 binary digits (1R1G2B) to bitsPerByteNeeded (4 bit == 0-15)
-		img.setRGB(0, 0, setRGBbits(img.getRGB(0, 0), bitsPerByteNeeded, 4));
-		int x=1,y=0;
-		boolean end = false;
-		
-		int k=0;
-		
-		if (bitsPerByteNeeded == 1){
-			
-			for (int i=0; i < message.length() && !end; i++){//Loop though each byte of message
-				byte b = (byte) message.charAt(i);
-				for (int j=7; j >= 0; j--){ //Loops though each bit of the byte
-					
-					
-					if (((b >> j) & 1) == 1)//if bit == 1
-						img.setRGB(x, y, img.getRGB(x,y) | (1<<(8*k))); //set R,G,or B rightmost bit to 1
-					else //if bit == 0
-						img.setRGB(x, y, img.getRGB(x,y) &~ (1<<(8*k))); //set R,G,or B rightmost bit to 0
-					
-					
-					k++;
-					if (k == 3){
-						k=0;
-						x++;
-						if (x >= img.getWidth()){ 
-							x=0; //Jump to next row if at the end
-							y++;
-							if (y >= img.getHeight()){
-								System.err.println("Message 2 large");
-								end = true;
-								break;
-							}
-						}
-					}
-				}
-			}
-		}
-		else{
-			
-			String endBit = "";
-			
-			for (int i=0; i < message.length() && !end; i++){//Loop though each byte of message
-				byte b = (byte) message.charAt(i);
-				
-				for (int j=7; j >= 0; j--){ //Loops though each bit of the byte
-					
-					if (endBit.length() < bitsPerByteNeeded*3){ //*3 for each color channel
-						endBit += ((b >> j) & 1);
-						//say(endBit);
-					}
-					else{
-						//say("before: ");
-						//sayBin(img.getRGB(x, y));
-						img.setRGB(x, y, setRGBbits(img.getRGB(x, y), Integer.parseInt(endBit,2), bitsPerByteNeeded*3));
-						//say("after: ");
-						//sayBin(img.getRGB(x, y));
-						
-						endBit = "";
-						x++;
-						if (x >= img.getWidth()){ 
-							x=0; //Jump to next row if at the end
-							y++;
-							if (y >= img.getHeight()){
-								System.err.println("Message too large");
-								end = true;
-								break;
-							}
-						}
-					}
-					
-					
-				}
-			}
-		}
-		
-		
-		
 		
 		try {
 			ImageIO.write(img, "png", new File("src/testSave.png")); //Save Image
@@ -142,24 +81,84 @@ public class Encrypt {
 
 	}
 	
+	static int x=1, y=0, k=0;
+	static boolean end = false;
+	
+	private static void singleBit(String line){
+		
+		for (int i=0; i < line.length() && !end; i++){//Loop though each byte of message
+			byte b = (byte) line.charAt(i);
+			
+			for (int j=7; j >= 0; j--){ //Loops though each bit of the byte
+				
+				if (((b >> j) & 1) == 1)//if bit == 1
+					img.setRGB(x, y, img.getRGB(x,y) | (1<<(8*k))); //set R,G,or B rightmost bit to 1
+				else //if bit == 0
+					img.setRGB(x, y, img.getRGB(x,y) &~ (1<<(8*k))); //set R,G,or B rightmost bit to 0
+				
+				k++;
+				if (k == 3){
+					k=0;
+					x++;
+					if (x >= img.getWidth()){ 
+						x=0; //Jump to next row if at the end
+						y++;
+						if (y >= img.getHeight()){
+							System.err.println("Message too large");
+							end = true;
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	static String endBit = "";
+	
+	private static void multiBit(String line){
+		
+		for (int i=0; i < line.length() && !end; i++){//Loop though each byte of message
+			byte b = (byte) line.charAt(i);
+			//say((char)b);
+			for (int j=7; j >= 0; j--){ //Loops though each bit of the byte
+				
+				//if (endBit.length() < bitsPerByteNeeded*3){ //*3 for each color channel
+					endBit += ((b >> j) & 1);
+					//say(endBit);
+				//}
+				/*else*/ if (endBit.length() == bitsPerByteNeeded*3){
+					//say("before: ");
+					//sayBin(img.getRGB(x, y));
+					img.setRGB(x, y, setRGBbits(img.getRGB(x, y), Integer.parseInt(new StringBuilder(endBit).reverse().toString(),2), bitsPerByteNeeded*3));
+					//say("after: ");
+					//sayBin(img.getRGB(x, y));
+					
+					endBit = "";
+					x++;
+					if (x >= img.getWidth()){ 
+						x=0; //Jump to next row if at the end
+						y++;
+						if (y >= img.getHeight()){
+							System.err.println("Message too large");
+							System.exit(0);
+							end = true;
+							break;
+						}
+					}
+				}
+				
+				
+			}
+		}
+		
+	}
+	
 	//TODO: less lazy/more efficient way of doing this
 	private static int getNthLastDigits(int num, int digits) {
 		String bin = Integer.toBinaryString(num);
 		return Integer.parseInt(bin.substring(bin.length()-digits),2);
 	}
-
-	/*private static int setLastBits(int num, int val, int digits){
-		
-		for (int i=0; i < digits; i++, val = val>>1){
-			
-			if ((val&1)==1)
-				num = (num | (1 << i));
-			else
-				num = num &~ (1 << i);
-		}
-			
-			return num;
-	}*/
 	
 	private static int setRGBbits(int rgb, int val, int digits){
 		
